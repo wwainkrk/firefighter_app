4044f23c0cce61ef13bcea1e3ac8734560fc0a97
Sebastian Warszawa <warsaw0902@gmail.com>
2020-05-22 18:38:26 +0200

    Build simply list without style, with items about each function in first point - Service

--- order_pdf.py
+++ order_pdf.py
@@ -1,5 +1,6 @@
 import datetime, time
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.platypus import ListFlowable, ListItem
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
@@ -35,5 +36,27 @@ elements.append(Spacer(1, 12))
 ptext = f'<font size="12">z dnia {date.day}.{date.month}.{date.year} r.</font>'
 elements.append(Paragraph(ptext, styles['Center']))
 elements.append(Spacer(1, 12))
+
+service_label = ['Dyżur domowy  Dowódcy Grupy ',
+                 'Dyżurny operacyjny Rejonu',
+                 'Oficer Operacyjny',
+                 'Dowódca zmiany',
+                 'Dyżurny PA JRG',
+                 'Szef zmiany',
+                 'Garażomistrz',
+                 'Dowódca działań ratowniczych SGRW-N',
+                 'Bosman',
+                 'Podoficer dyżurny',
+                 'Strażak dyżurny'
+                ]
+
+service_list = []
+
+for label in service_label:
+    list_item = ListItem(Paragraph(label, styles['Left']), value=False)
+    service_list.append(list_item)
+
+service = ListFlowable(service_list, leftIndent=37.5)
+elements.append(service)
 
 doc.build(elements)
