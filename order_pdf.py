c7fcf9564a2c5ae71191e37e8a5f7b90ba677a27
Sebastian Warszawa <warsaw0902@gmail.com>
2020-10-20 21:52:58 +0200

    Register new Font, which handdle with Polish characters; changes in Styles for headers and rest of document, Simply footer Paragraph at the end of the document

--- order_pdf.py
+++ order_pdf.py
@@ -5,6 +5,9 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
 # Creating document
 doc = SimpleDocTemplate("firefighter_test.pdf", pagesize=letter,
                         rightMargin=9, leftMargin=25,
@@ -13,11 +16,18 @@ doc = SimpleDocTemplate("firefighter_test.pdf", pagesize=letter,
 # Elements on page which will be print
 elements = []
 
+# Register font and set style with new Font
+pdfmetrics.registerFont(TTFont('FreeSans', "fonts/FreeSans.ttf"))
+pdfmetrics.registerFont(TTFont('FreeSansBold', "fonts/FreeSansBold.ttf"))
+pdfmetrics.registerFont(TTFont('FreeSansItalic', "fonts/FreeSansOblique.ttf"))
+
 # Setting additional styles for document
 styles = getSampleStyleSheet()
-styles.add(ParagraphStyle(name='Left', fontName='Times'))
-styles.add(ParagraphStyle(name='Center', fontName='Times', alignment=TA_CENTER))    # style for good looking document
-styles.add(ParagraphStyle(name='Right', fontName='Times', alignment=TA_RIGHT))        # style to set paragraph on right side of page
+styles.add(ParagraphStyle(name='Left', fontName='FreeSans'))
+styles.add(ParagraphStyle(name='HeaderBoldCenter', fontName='FreeSansBold', alignment=TA_CENTER))    # style for good looking document
+styles.add(ParagraphStyle(name='HeaderCenter', fontName='FreeSans', alignment=TA_CENTER))
+styles.add(ParagraphStyle(name='Right', fontName='FreeSans', alignment=TA_RIGHT))        # style to set paragraph on right side of page
+styles.add(ParagraphStyle(name='FooterBoldRight', fontName='FreeSansBold', alignment=TA_RIGHT))
 
 date = datetime.datetime.now().date()
 ptext = f'<font size="12">Kraków, dn. {date.day}.{date.month}.{date.year}</font>'
@@ -26,15 +36,15 @@ elements.append(Spacer(1, 12))
 
 day_of_year = time.localtime().tm_yday
 ptext = f'<font size="16"><b>ROZKAZ DZIENNY NR {day_of_year}/{date.year} r.</b></font>'
-elements.append(Paragraph(ptext, styles['Center']))
+elements.append(Paragraph(ptext, styles['HeaderBoldCenter']))
 elements.append(Spacer(1, 12))
 
 ptext = '<font size="12"><b>Dowódcy Jednostki Ratunkowo Gaśniczej Nr 4</b></font>'
-elements.append(Paragraph(ptext, styles['Center']))
+elements.append(Paragraph(ptext, styles['HeaderBoldCenter']))
 elements.append(Spacer(1, 12))
 
 ptext = f'<font size="12">z dnia {date.day}.{date.month}.{date.year} r.</font>'
-elements.append(Paragraph(ptext, styles['Center']))
+elements.append(Paragraph(ptext, styles['HeaderCenter']))
 elements.append(Spacer(1, 12))
 
 service_label = ['Dyżur domowy  Dowódcy Grupy ',
@@ -58,5 +68,8 @@ for label in service_label:
 
 service = ListFlowable(service_list, leftIndent=37.5)
 elements.append(service)
+
+ptext = f'<font size="14">Podpisał D-ca JRG-4</font>'
+elements.append(Paragraph(ptext, styles['FooterBoldRight']))
 
 doc.build(elements)
