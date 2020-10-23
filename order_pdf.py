e3c66c14a42a47749b92d29dd9aea5b17dc4a937
Sebastian Warszawa <warsaw0902@gmail.com>
2020-10-23 23:21:52 +0200

    Refactor code, first attempt to split code into functions

--- order_pdf.py
+++ order_pdf.py
@@ -3,73 +3,103 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.platypus import ListFlowable, ListItem
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
-from reportlab.lib.enums import TA_CENTER, TA_RIGHT
+from reportlab.lib.enums import TA_CENTER, TA_RIGHT, TA_LEFT
 
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.fonts import addMapping
 
-# Creating document
-doc = SimpleDocTemplate("firefighter_test.pdf", pagesize=letter,
-                        rightMargin=9, leftMargin=25,
-                        topMargin=20, bottomMargin=20)          # constructor of document with settings for margins
 
-# Elements on page which will be print
-elements = []
+def create_pdf():
+    # Creating document
+    doc = SimpleDocTemplate("firefighter_test.pdf", pagesize=letter,
+                            rightMargin=20, leftMargin=20,
+                            topMargin=20, bottomMargin=20)          # constructor of document with settings for margins
 
-# Register font and set style with new Font
-pdfmetrics.registerFont(TTFont('FreeSans', "fonts/FreeSans.ttf"))
-pdfmetrics.registerFont(TTFont('FreeSansBold', "fonts/FreeSansBold.ttf"))
-pdfmetrics.registerFont(TTFont('FreeSansItalic', "fonts/FreeSansOblique.ttf"))
+    # Elements on page which will be print
+    doc_elements = []
+    register_fonts()
+    doc_styles = set_styles()
+    create_header(doc_elements, doc_styles)
+    create_sections(doc_elements, doc_styles)
+    doc.build(doc_elements)
 
-# Setting additional styles for document
-styles = getSampleStyleSheet()
-styles.add(ParagraphStyle(name='Left', fontName='FreeSans'))
-styles.add(ParagraphStyle(name='HeaderBoldCenter', fontName='FreeSansBold', alignment=TA_CENTER))    # style for good looking document
-styles.add(ParagraphStyle(name='HeaderCenter', fontName='FreeSans', alignment=TA_CENTER))
-styles.add(ParagraphStyle(name='Right', fontName='FreeSans', alignment=TA_RIGHT))        # style to set paragraph on right side of page
-styles.add(ParagraphStyle(name='FooterBoldRight', fontName='FreeSansBold', alignment=TA_RIGHT))
 
-date = datetime.datetime.now().date()
-ptext = f'<font size="12">Kraków, dn. {date.day}.{date.month}.{date.year}</font>'
-elements.append(Paragraph(ptext, styles['Right']))
-elements.append(Spacer(1, 12))
+def register_fonts():
+    # Register font and set style with new Font
+    pdfmetrics.registerFont(TTFont('FreeSans', "fonts/FreeSans.ttf"))
+    pdfmetrics.registerFont(TTFont('FreeSansBold', "fonts/FreeSansBold.ttf"))
+    pdfmetrics.registerFont(TTFont('FreeSansItalic', "fonts/FreeSansOblique.ttf"))
+    pdfmetrics.registerFont(TTFont('FreeSansBoldItalic', "fonts/FreeSansBoldOblique.ttf"))
 
-day_of_year = time.localtime().tm_yday
-ptext = f'<font size="16"><b>ROZKAZ DZIENNY NR {day_of_year}/{date.year} r.</b></font>'
-elements.append(Paragraph(ptext, styles['HeaderBoldCenter']))
-elements.append(Spacer(1, 12))
+    pdfmetrics.registerFontFamily('FreeSans', normal='FreeSans', bold='FreeSansBold', italic='FreeSansItalic', boldItalic='FreeSansBoldItalic')
+    addMapping('FreeSans', 0, 0, 'FreeSans')
+    addMapping('FreeSans', 0, 1, 'FreeSansItalic')
+    addMapping('FreeSans', 1, 0, 'FreeSansBold')
+    addMapping('FreeSans', 1, 1, 'FreeSansBoldItalic')
 
-ptext = '<font size="12"><b>Dowódcy Jednostki Ratunkowo Gaśniczej Nr 4</b></font>'
-elements.append(Paragraph(ptext, styles['HeaderBoldCenter']))
-elements.append(Spacer(1, 12))
 
-ptext = f'<font size="12">z dnia {date.day}.{date.month}.{date.year} r.</font>'
-elements.append(Paragraph(ptext, styles['HeaderCenter']))
-elements.append(Spacer(1, 12))
+def set_styles():
+    # Setting additional styles for document
+    styles = getSampleStyleSheet()
+    #styles.add(ParagraphStyle(name='BoldLeft', fontName='FreeSansBold', alignment=TA_LEFT))
+    styles.add(ParagraphStyle(name='Left', fontName='FreeSans', alignment=TA_LEFT))
+    #styles.add(ParagraphStyle(name='HeaderBoldCenter', fontName='FreeSansBold', alignment=TA_CENTER))    # style for good looking document
+    styles.add(ParagraphStyle(name='Center', fontName='FreeSans', alignment=TA_CENTER))
+    styles.add(ParagraphStyle(name='Right', fontName='FreeSans', alignment=TA_RIGHT))        # style to set paragraph on right side of page
+    #styles.add(ParagraphStyle(name='FooterBoldRight', fontName='FreeSansBold', alignment=TA_RIGHT))
 
-service_label = ['Dyżur domowy  Dowódcy Grupy ',
-                 'Dyżurny operacyjny Rejonu',
-                 'Oficer Operacyjny',
-                 'Dowódca zmiany',
-                 'Dyżurny PA JRG',
-                 'Szef zmiany',
-                 'Garażomistrz',
-                 'Dowódca działań ratowniczych SGRW-N',
-                 'Bosman',
-                 'Podoficer dyżurny',
-                 'Strażak dyżurny'
-                ]
+    #print(styles.list())
+    return styles
 
-service_list = []
 
-for label in service_label:
-    list_item = ListItem(Paragraph(label, styles['Left']), value=False)
-    service_list.append(list_item)
+def create_header(elements, styles):
+    date = datetime.datetime.now().date()
+    ptext = f'<font size="12"><i>Kraków, dn. {date.day}.{date.month}.{date.year}</i></font>'
+    elements.append(Paragraph(ptext, styles['Right']))
+    elements.append(Spacer(1, 12))
 
-service = ListFlowable(service_list, leftIndent=37.5)
-elements.append(service)
+    day_of_year = time.localtime().tm_yday
+    ptext = f'<font size="16">ROZKAZ DZIENNY NR {day_of_year}/{date.year} r.</font>'
+    elements.append(Paragraph(ptext, styles['Center']))
+    elements.append(Spacer(1, 12))
 
-ptext = f'<font size="14">Podpisał D-ca JRG-4</font>'
-elements.append(Paragraph(ptext, styles['FooterBoldRight']))
+    ptext = '<font size="12"><b>Dowódcy Jednostki Ratunkowo Gaśniczej Nr 4</b></font>'
+    elements.append(Paragraph(ptext, styles['Center']))
+    elements.append(Spacer(1, 12))
 
-doc.build(elements)
+    ptext = f'<font size="12">z dnia {date.day}.{date.month}.{date.year} r.</font>'
+    elements.append(Paragraph(ptext, styles['Center']))
+    elements.append(Spacer(1, 12))
+
+
+def create_sections(elements, styles):
+    service_label = ['Dyżur domowy  Dowódcy Grupy ',
+                     'Dyżurny operacyjny Rejonu',
+                     'Oficer Operacyjny',
+                     'Dowódca zmiany',
+                     'Dyżurny PA JRG',
+                     'Szef zmiany',
+                     'Garażomistrz',
+                     'Dowódca działań ratowniczych SGRW-N',
+                     'Bosman',
+                     'Podoficer dyżurny',
+                     'Strażak dyżurny'
+                    ]
+
+    service_list = []
+
+    counter = 1
+    for label in service_label:
+        list_item = ListItem(Paragraph(f"{counter}. {label}", styles['Left']), value=False)
+        service_list.append(list_item)
+        counter += 1
+
+    service = ListFlowable(service_list, leftIndent=37.5)
+    elements.append(service)
+
+    ptext = f'<font size="14"><b>Podpisał D-ca JRG-4</b></font>'
+    elements.append(Paragraph(ptext, styles['Right']))
+
+
+create_pdf()
